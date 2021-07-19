015b6a5cb8299989dddd0fb685681942a15606ae
elektrW <wojdom1@wp.pl>
2021-07-19 22:43:00 +0200

    Changed type for main key 'id' (hours and employee) from long to Long

--- src/main/java/pl/wojciechdomagala/enterhourwebapp/model/Employee.java
+++ src/main/java/pl/wojciechdomagala/enterhourwebapp/model/Employee.java
@@ -9,7 +9,7 @@ public class Employee {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
-    private long id;
+    private Long id;
 
     @Column(name = "first_name")
     private String firstName;
@@ -20,11 +20,11 @@ public class Employee {
     @Column(name = "email")
     private String email;
 
-    public long getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(long id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
--- src/main/java/pl/wojciechdomagala/enterhourwebapp/model/Hours.java
+++ src/main/java/pl/wojciechdomagala/enterhourwebapp/model/Hours.java
@@ -9,7 +9,7 @@ public class Hours {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_hours")
-    private long id;
+    private Long id;
 
     @Column(name = "Name_of_place")
     private String nameOfPlace;
@@ -20,11 +20,11 @@ public class Hours {
     @Column(name = "Hours")
     private Integer hours;
 
-    public long getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(long id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
